311d50272e365013548454f8e142275c56ea8af7
Heinrich <heinrich.edric@cs.ui.ac.id>
2024-09-10 22:11:26 +0700

    Final hopefully

--- main/views.py
+++ main/views.py
@@ -1,10 +1,21 @@
+import csv
 from django.shortcuts import render
+import os
 
 def show_main(request):
+    products = []
+    file_path = os.path.join(os.path.dirname(__file__),'Products.csv')
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            products.append({
+                'product': row.get('products', 'N/A'),
+                'price': row.get('price', 'N/A'),
+                'desc': row.get('desc', 'N/A')
+            })
+
     context = {
-        'price' : '23.000,00',
-        'product': 'Bintang 0,0%',
-        'desc': 'Non-alcoholic'
+        'products': products
     }
 
     return render(request, "main.html", context)
